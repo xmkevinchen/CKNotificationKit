0b3a76772dde5a9ae9d4617d1ac6efb8ff39f67d
Kevin Chen <xmkevinchen@gmail.com>
2015-12-31 15:34:01 -0800

    - Initialized CKNotificationKit repository

---
+++ CKNotificationKit/CKNotificationKit.h
@@ -0,0 +1,19 @@
+//
+//  CKNotificationKit.h
+//  CKNotificationKit
+//
+//  Created by Kevin Chen on 12/31/15.
+//  Copyright © 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CKNotificationKit.
+FOUNDATION_EXPORT double CKNotificationKitVersionNumber;
+
+//! Project version string for CKNotificationKit.
+FOUNDATION_EXPORT const unsigned char CKNotificationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKNotificationKit/PublicHeader.h>
+
+
